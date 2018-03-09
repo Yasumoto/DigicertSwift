8d29f4f6fb8f1c9fc10691ebca9f11da2deb8095
Joe Smith <yasumoto7@gmail.com>
2018-03-08 17:02:48 -0800

    Start to submit wildcard

--- Sources/DigicertSwift/DigicertSwift.swift
+++ Sources/DigicertSwift/DigicertSwift.swift
@@ -2,53 +2,7 @@ import Dispatch
 import Foundation
 
 public enum DigicertError: Error {
-    case eventParsingError
-}
-
-public struct Page: Codable {
-    public let total: Int
-    public let limit: Int
-    public let offset: Int
-}
-
-public struct Product: Codable {
-    public let name_id: String
-    public let name: String
-    public let type: String
-}
-
-public struct Container: Codable {
-    public let id: Int
-    public let name: String
-}
-
-public struct Organization: Codable {
-    public let id: Int
-    public let name: String
-}
-
-public struct Certificate: Codable {
-    public let common_name: String?
-    public let dns_names: [String]?
-    public let valid_till: String //TODO DateFormatter
-    public let signature_hash: String
-}
-
-public struct Order: Codable {
-    public let id: Int
-    public let certificate: Certificate
-    public let status: String
-    public let date_created: Date
-    public let organization: Organization
-    public let validity_years: Int
-    public let container: Container
-    public let product: Product
-    public let price: Float?
-}
-
-public struct Orders: Codable {
-    public let orders: [Order]
-    public let page: Page
+    case responseParsingError
 }
 
 public struct DigicertSwift {
@@ -64,6 +18,7 @@ public struct DigicertSwift {
     func submitRequest(path: String,
                        parameters: [String:String] = [:],
                        method: String = "GET",
+                       body: Data? = nil,
                        debug: Bool = false) -> Data? {
         var response: Data? = nil
         var requestString = "\(baseDomain)/\(path)"
@@ -77,6 +32,10 @@ public struct DigicertSwift {
             var request = URLRequest(url: requestURL)
 
             request.httpMethod = method
+            if let requestBody = body {
+                request.httpBody = requestBody
+                //request.setValue("\(requestBody.)", forHTTPHeaderField: "Content-Length")
+            }
             request.setValue("application/json", forHTTPHeaderField: "Accept")
             request.setValue(apiKey, forHTTPHeaderField: authHeader)
             let task = session.dataTask(with: request) {
@@ -101,6 +60,54 @@ public struct DigicertSwift {
         }
         return response
     }
+}
+
+extension DigicertSwift {
+    public struct Page: Codable {
+        public let total: Int
+        public let limit: Int
+        public let offset: Int
+    }
+
+    public struct Product: Codable {
+        public let name_id: String
+        public let name: String
+        public let type: String
+    }
+
+    public struct Container: Codable {
+        public let id: Int
+        public let name: String
+    }
+
+    public struct Organization: Codable {
+        public let id: Int
+        public let name: String
+    }
+
+    public struct Certificate: Codable {
+        public let common_name: String?
+        public let dns_names: [String]?
+        public let valid_till: String //TODO DateFormatter
+        public let signature_hash: String
+    }
+
+    public struct Order: Codable {
+        public let id: Int
+        public let certificate: Certificate
+        public let status: String
+        public let date_created: Date
+        public let organization: Organization
+        public let validity_years: Int
+        public let container: Container
+        public let product: Product
+        public let price: Float?
+    }
+
+    public struct Orders: Codable {
+        public let orders: [Order]
+        public let page: Page
+    }
 
     /**
      List all certificate orders
@@ -110,7 +117,7 @@ public struct DigicertSwift {
     public func listOrders() throws -> [Order]? {
         var orders: [Order]? = nil
 
-        if let response = submitRequest(path: "order/certificate", debug: true) {
+        if let response = submitRequest(path: "order/certificate") {
             do {
                 let decoder = JSONDecoder()
                 if #available(OSX 10.12, *) {
@@ -121,11 +128,96 @@ public struct DigicertSwift {
                 }
                 orders = try decoder.decode(Orders.self, from: response).orders
             } catch {
-                print("Could not parse events: \(error)")
-                throw DigicertError.eventParsingError
+                print("Could not parse order list: \(error)")
+                throw DigicertError.responseParsingError
             }
         }
 
         return orders
+    }
+}
+
+extension DigicertSwift {
+    enum SignatureHash: String, Codable {
+        case sha256, sha384, sha512
+    }
+
+    struct ServerPlatform: Codable {
+        let id: Int
+    }
+
+    struct CertificateRequest: Codable {
+        let common_name: String
+        let csr: String
+        let organization_units: [String]?
+        let server_platform: ServerPlatform?
+        let signature_hash: SignatureHash
+        let profile_option: String?
+    }
+
+    struct RequestOrganization: Codable {
+        let id: Int
+    }
+
+    struct WildcardRequest: Codable {
+        let certificate: CertificateRequest
+        let organization: RequestOrganization
+        let validity_years: Int
+        let custom_expiration_date: String? //TODO Make this YYYY-MM-DD format
+        let comments: String?
+        let disable_renewal_notifications: Bool?
+        let renewal_of_order_id: Int?
+        let disable_ct: Bool?
+    }
+
+    public enum RequestStatus: String, Codable {
+        case pending, approved, rejected
+    }
+
+    public struct Request: Codable {
+        public let id: Int
+        public let status: RequestStatus
+    }
+
+    public struct WildcardResponse: Codable {
+        public let id: Int
+        public let requests: [Request]
+    }
+
+    /**
+     Request a new Wildcard certificate
+
+     [API Documentation](https://www.digicert.com/services/v2/documentation/order/order-ssl-wildcard)
+
+     - parameters
+        - name: Title of the event
+        - description: Details for the meetup
+        - duration: defaults to 3 hours (10800000)
+        - startTime: Starting time for event
+     */
+    public func requestWildcard(commonName: String,
+                                csr: String,
+                                duration: Int,
+                                startTime: Date,
+                                venue: Int) throws -> WildcardResponse? {
+        let requestOrganization = RequestOrganization(id: 5)
+        let certificate = CertificateRequest.init(common_name: commonName, csr: csr, organization_units: nil, server_platform: nil, signature_hash: .sha256, profile_option: nil)
+        let request = WildcardRequest(certificate: certificate, organization: requestOrganization, validity_years: 1, custom_expiration_date: nil, comments: nil, disable_renewal_notifications: nil, renewal_of_order_id: nil, disable_ct: nil)
+        let body = try JSONEncoder().encode(request)
+        print(request)
+        print(body)
+        return nil/*
+        if let response = submitRequest(path: "certificate/ssl_wildcard",
+                                        method: "POST",
+                                        body: body,
+                                        debug: true) {
+            do {
+                return try JSONDecoder().decode(WildcardResponse.self, from: response)
+            } catch {
+                print("Couldn't parse response: \(error)")
+                throw DigicertError.responseParsingError
+            }
+        }
+        return nil*/
     }
 }
