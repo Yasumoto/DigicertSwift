fe97ef3ec544972cb91b64c1654d23a2b844d2df
Joe Smith <yasumoto7@gmail.com>
2018-03-09 10:22:55 -0800

    Little reformatting, and turns out standard accounts can't list orgs

--- Sources/DigicertSwift/DigicertSwift.swift
+++ Sources/DigicertSwift/DigicertSwift.swift
@@ -3,6 +3,16 @@ import Foundation
 
 public enum DigicertError: Error {
     case responseParsingError
+    case networkError(error: DigicertNetworkError)
+}
+
+public struct DigicertNetworkError: Error, Codable {
+    public let code: String
+    public let message: String
+}
+
+public struct DigicertNetworkErrors: Codable {
+    public let errors: [DigicertNetworkError]
 }
 
 public struct DigicertSwift {
@@ -10,27 +20,28 @@ public struct DigicertSwift {
     let authHeader = "X-DC-DEVKEY"
     let sema = DispatchSemaphore(value: 0)
     let apiKey: String
-
+    
     public init(apiKey: String) {
         self.apiKey = apiKey
     }
-
+    
     func submitRequest(path: String,
                        parameters: [String:String] = [:],
                        method: String = "GET",
                        body: Data? = nil,
                        debug: Bool = false) -> Data? {
         var response: Data? = nil
-        var requestString = "\(baseDomain)/\(path)"
-
+        var errorResponse: DigicertNetworkError? = nil
+        var requestString = "\(baseDomain)\(path)"
+        
         for (key, value) in parameters {
             requestString.append("&\(key)=\(value)")
         }
-
+        
         if let requestURL = URL(string: requestString) {
             let session = URLSession(configuration: URLSessionConfiguration.default)
             var request = URLRequest(url: requestURL)
-
+            
             request.httpMethod = method
             if let requestBody = body {
                 request.httpBody = requestBody
@@ -42,6 +53,16 @@ public struct DigicertSwift {
                 if let responded = $1 as? HTTPURLResponse {
                     if responded.statusCode != 200 {
                         print("Non-200 response was: \(responded)")
+                        if let data = $0 {
+                            do {
+                                let errors = try JSONDecoder().decode(DigicertNetworkErrors.self, from: data)
+                                if let error = errors.errors.first {
+                                    errorResponse = error
+                                }
+                            } catch {
+                                print("Error decoding Digicert error: \(error)")
+                            }
+                        }
                     }
                     if let responseError = $2 {
                         if debug {
@@ -58,6 +79,10 @@ public struct DigicertSwift {
             task.resume()
             sema.wait()
         }
+        if let responseError = errorResponse {
+            print("Error: \(responseError)")
+            return nil
+        }
         return response
     }
 }
@@ -68,55 +93,55 @@ extension DigicertSwift {
         public let limit: Int
         public let offset: Int
     }
-
+    
     public struct Product: Codable {
         public let name_id: String
         public let name: String
         public let type: String
     }
-
+    
     public struct Container: Codable {
         public let id: Int
         public let name: String
     }
-
-    public struct Organization: Codable {
+    
+    public struct OrderOrganization: Codable {
         public let id: Int
         public let name: String
     }
-
+    
     public struct Certificate: Codable {
         public let common_name: String?
         public let dns_names: [String]?
         public let valid_till: String //TODO DateFormatter
         public let signature_hash: String
     }
-
+    
     public struct Order: Codable {
         public let id: Int
         public let certificate: Certificate
         public let status: String
         public let date_created: Date
-        public let organization: Organization
+        public let organization: OrderOrganization
         public let validity_years: Int
         public let container: Container
         public let product: Product
         public let price: Float?
     }
-
+    
     public struct Orders: Codable {
         public let orders: [Order]
         public let page: Page
     }
-
+    
     /**
      List all certificate orders
-
+     
      https://www.digicert.com/services/v2/documentation/order/order-list
      */
     public func listOrders() throws -> [Order]? {
         var orders: [Order]? = nil
-
+        
         if let response = submitRequest(path: "order/certificate") {
             do {
                 let decoder = JSONDecoder()
@@ -132,92 +157,128 @@ extension DigicertSwift {
                 throw DigicertError.responseParsingError
             }
         }
-
+        
         return orders
     }
+}
+
+extension DigicertSwift {
+    struct Organization: Codable {
+        public let id: Int
+        
+        private enum CodingKeys: String, CodingKey {
+            case id
+        }
+    }
+    
+    struct Organizations: Codable {
+        public let organizations: [Organization]
+    }
+    
+    /**
+     List all organizations associated with this account
+
+     *TODO* Not quite ready for use yet
+     
+     https://www.digicert.com/services/v2/documentation/organization/organization-list
+     */
+    private func listOrganizations() throws -> [Organization]? {
+        var orders: [Organization]? = nil
+        
+        if let response = submitRequest(path: "organization", debug: true) {
+            do {
+                orders = try JSONDecoder().decode(Organizations.self, from: response).organizations
+            } catch {
+                print("Could not parse organization list: \(error)")
+                throw DigicertError.responseParsingError
+            }
+        }
+        
+        return orders
+    }
+    
 }
 
 extension DigicertSwift {
     enum SignatureHash: String, Codable {
         case sha256, sha384, sha512
     }
-
+    
     struct ServerPlatform: Codable {
         let id: Int
     }
-
+    
     struct CertificateRequest: Codable {
         let common_name: String
         let csr: String
-        let organization_units: [String]?
-        let server_platform: ServerPlatform?
         let signature_hash: SignatureHash
-        let profile_option: String?
+        let organization_units: [String]? = nil
+        let server_platform: ServerPlatform? = nil
+        let profile_option: String? = nil
     }
-
+    
     struct RequestOrganization: Codable {
         let id: Int
     }
-
+    
     struct WildcardRequest: Codable {
         let certificate: CertificateRequest
         let organization: RequestOrganization
         let validity_years: Int
-        let custom_expiration_date: String? //TODO Make this YYYY-MM-DD format
-        let comments: String?
-        let disable_renewal_notifications: Bool?
-        let renewal_of_order_id: Int?
-        let disable_ct: Bool?
+        let custom_expiration_date: String? = nil //TODO Make this YYYY-MM-DD format
+        let comments: String? = nil
+        let disable_renewal_notifications: Bool? = nil
+        let renewal_of_order_id: Int? = nil
+        let disable_ct: Bool? = nil
     }
-
+    
     public enum RequestStatus: String, Codable {
         case pending, approved, rejected
     }
-
+    
     public struct Request: Codable {
         public let id: Int
         public let status: RequestStatus
     }
-
+    
     public struct WildcardResponse: Codable {
         public let id: Int
         public let requests: [Request]
     }
-
+    
     /**
      Request a new Wildcard certificate
-
+     
      [API Documentation](https://www.digicert.com/services/v2/documentation/order/order-ssl-wildcard)
-
+     
      - parameters
-        - name: Title of the event
-        - description: Details for the meetup
-        - duration: defaults to 3 hours (10800000)
-        - startTime: Starting time for event
+        - commonName: Name of the certificate
+        - csr: Full Certificate Signing Request as a string
+        - organizationId: Get your organization ID from the web UI
+        - validityYears: Number of years for the cert to be valid
      */
     public func requestWildcard(commonName: String,
                                 csr: String,
-                                duration: Int,
-                                startTime: Date,
-                                venue: Int) throws -> WildcardResponse? {
-        let requestOrganization = RequestOrganization(id: 5)
-        let certificate = CertificateRequest.init(common_name: commonName, csr: csr, organization_units: nil, server_platform: nil, signature_hash: .sha256, profile_option: nil)
-        let request = WildcardRequest(certificate: certificate, organization: requestOrganization, validity_years: 1, custom_expiration_date: nil, comments: nil, disable_renewal_notifications: nil, renewal_of_order_id: nil, disable_ct: nil)
+                                organizationId: Int,
+                                validityYears: Int = 1) throws -> WildcardResponse? {
+        let requestOrganization = RequestOrganization(id: organizationId)
+        let certificate = CertificateRequest(common_name: commonName, csr: csr, signature_hash: .sha256)
+        let request = WildcardRequest(certificate: certificate, organization: requestOrganization, validity_years: validityYears)
         let body = try JSONEncoder().encode(request)
         print(request)
         print(body)
         return nil/*
-        if let response = submitRequest(path: "certificate/ssl_wildcard",
-                                        method: "POST",
-                                        body: body,
-                                        debug: true) {
-            do {
-                return try JSONDecoder().decode(WildcardResponse.self, from: response)
-            } catch {
-                print("Couldn't parse response: \(error)")
-                throw DigicertError.responseParsingError
-            }
-        }
-        return nil*/
+         if let response = submitRequest(path: "certificate/ssl_wildcard",
+         method: "POST",
+         body: body,
+         debug: true) {
+         do {
+         return try JSONDecoder().decode(WildcardResponse.self, from: response)
+         } catch {
+         print("Couldn't parse response: \(error)")
+         throw DigicertError.responseParsingError
+         }
+         }
+         return nil*/
     }
 }
